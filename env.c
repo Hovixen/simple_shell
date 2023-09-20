0869ffb97f7af601746f19d9cb2439f21796b4d0
Bokang06 <bokangshogole45@gmail.com>
2023-09-20 08:18:27 -0700

    env

--- env.c
+++ env.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 
-char *get_env (const char *env_name)
+char *get_env(const char *env_name)
 {
 	int len = 0;
 	int i;
 	char *_env;
+
 	for (i = 0; environ[i]; i++)
 	{
 		_env = environ[i];
 		len = _strlen(env_name);
-		
 		if (strncmp(_env, env_name, len) == 0 && _env[len] == '=')
 			return (&_env[len + 1]);
 	}
